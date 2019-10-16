84a6f4aac8438b0c39140376c9e78d7d1249ba73
Joseph Early <joseph.early@btinternet.com>
2019-10-16 14:18:36 +0100

    Extracted submission code into utils.

--- src/baseline.py
+++ src/baseline.py
@@ -1,3 +1,5 @@
+from utils import create_submission
+
 import pandas as pd
 import os
 
@@ -5,10 +7,4 @@ import os
 class_distribution = pd.read_csv('../data/train_labels.csv').groupby(['verified']).mean()[-1:].values
 
 # Create submission based on training class distribution
-submission = pd.read_csv('../data/submission_format.csv', index_col='id')
-submission.loc[:, 0:5] = class_distribution
-
-submission_folder = '../submissions'
-if not os.path.exists(submission_folder):
-    os.makedirs(submission_folder)
-submission.to_csv(submission_folder + '/baseline.csv')
+create_submission(class_distribution, "baseline")
