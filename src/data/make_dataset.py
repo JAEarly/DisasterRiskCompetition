4456231e8cd7b1672016813e58a4d1f0c15c5c2a
Joseph Early <joseph.early@btinternet.com>
2019-11-04 19:35:43 +0000

    Tidy up make dataset. Data dir done.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,36 +1,48 @@
-from tqdm import tqdm
-import requests
-import tarfile
+"""Dataset setup - download and extract from tar file."""
+
 import os
+import tarfile
 
+import requests
+from tqdm import tqdm
 
-DATASET_URL = "https://drivendata-public-assets-eu.s3.eu-central-1.amazonaws.com/stac.tar"
+DATASET_URL = (
+    "https://drivendata-public-assets-eu.s3.eu-central-1.amazonaws.com/stac.tar"
+)
 DATASET_PATH = "data/raw/stac.tar"
 EXTRACT_PATH = "data/raw/"
 
 
-def download_dataset():
+def download_dataset() -> None:
+    """
+    Download the dataset from the internet. Takes a while.
+    :return: None.
+    """
     # Only download dataset if not found
     if not os.path.exists(DATASET_PATH):
         print("Downloading dataset")
-        r = requests.get(DATASET_URL, stream=True)
+        request = requests.get(DATASET_URL, stream=True)
 
         # Total size in bytes.
-        total_size = int(r.headers.get('content-length', 0))
+        total_size = int(request.headers.get("content-length", 0))
         block_size = 1024
 
         # Download with tqdm progress bar
-        t = tqdm(total=total_size, unit='iB', unit_scale=True)
-        with open(DATASET_PATH, 'wb') as f:
-            for data in r.iter_content(block_size):
-                t.update(len(data))
-                f.write(data)
-        t.close()
+        p_bar = tqdm(total=total_size, unit="iB", unit_scale=True)
+        with open(DATASET_PATH, "wb") as output_file:
+            for data in request.iter_content(block_size):
+                p_bar.update(len(data))
+                output_file.write(data)
+        p_bar.close()
     else:
         print("Dataset file found, skipping download")
 
 
-def extract_dataset():
+def extract_dataset() -> None:
+    """
+    Extract dataset from tar file.
+    :return: None.
+    """
     # Only extract dataset if not already extracted
     if not os.path.exists(os.path.join(EXTRACT_PATH, "stac")):
         print("Extracting dataset")
