70924c3b5fdd9585584501a3df6bbee346ddf983
Joseph Early <joseph.early@btinternet.com>
2019-11-05 13:21:59 +0000

    Cleaned up submission file.

--- src/evaluation/submit.py
+++ src/evaluation/submit.py
@@ -1,48 +1,39 @@
+"""Script for creating submissions."""
+
 import os
 
 import pandas as pd
-from tqdm import tqdm
 import torch
+from tqdm import tqdm
+
 import models
-from features import CompetitionFeatureDataset, FeatureExtractor, AlexNet256
+from features import CompetitionFeatureDataset, AlexNet256, CompetitionDataset
 from utils import create_timestamp_str
 
-SUBMISSION_FOLDER = './submissions'
-SUBMISSION_FORMAT_PATH = './data/raw/submission_format.csv'
-COMP_DATASET_DIR = "./data/processed/competition"
-BATCH_SIZE = 8
+SUBMISSION_FOLDER = "./submissions"
+SUBMISSION_FORMAT_PATH = "./data/raw/submission_format.csv"
 
 
-# def create_submission_from_model(model: models.Model) -> None:
-#     competition_dataset = CompetitionDataset(COMP_DATASET_DIR, transform=model.get_transform())
-#     dataloader = DataLoader(competition_dataset, batch_size=BATCH_SIZE, shuffle=False)
-#
-#     competition_labels = []
-#     for batch in tqdm(dataloader, desc="Predicting competition dataset"):
-#         competition_labels.extend(model.predict_batch(batch))
-#
-#     # Get correct submission format from example submission file
-#     submission = pd.read_csv(SUBMISSION_FORMAT_PATH, index_col='id')
-#     # Add test labels for this submission
-#     submission.loc[:, 0:5] = competition_labels
-#
-#     # Actually write to csv file
-#     if not os.path.exists(SUBMISSION_FOLDER):
-#         os.makedirs(SUBMISSION_FOLDER)
-#     filename = model.name + "_" + create_timestamp_str() + ".csv"
-#     submission.to_csv(os.path.join(SUBMISSION_FOLDER, filename))
-
-def create_submission_from_features(model: models.Model, feature_extractor: FeatureExtractor):
-    competition_dataset = CompetitionFeatureDataset(feature_extractor)
-
+def create_submission(
+    model: models.Model, competition_dataset: CompetitionDataset
+) -> None:
+    """
+    Create a submission.
+    :param model: Model to use.
+    :param competition_dataset: Competition dataset to use.
+    :return: None.
+    """
+    # Get predicted labels for competition data
     competition_labels = []
-    for batch in tqdm(competition_dataset.data_loader, desc="Predicting competition dataset"):
+    for batch in tqdm(
+        competition_dataset.data_loader, desc="Predicting competition dataset"
+    ):
         y_outputs = model.predict_batch(batch)
         y_probas = torch.softmax(y_outputs, 1)
         competition_labels.extend(y_probas.cpu().detach().numpy())
 
     # Get correct submission format from example submission file
-    submission = pd.read_csv(SUBMISSION_FORMAT_PATH, index_col='id')
+    submission = pd.read_csv(SUBMISSION_FORMAT_PATH, index_col="id")
 
     # Add test labels for this submission
     submission.loc[:, 0:5] = competition_labels
@@ -55,5 +46,10 @@ def create_submission_from_features(model: models.Model, feature_extractor: Feat
 
 
 if __name__ == "__main__":
-    _model = models.NNModel(9216, state_dict_path="./models/basic_nn_2019-11-01_17:12:59.pth", eval_mode=True)
-    create_submission_from_features(_model, AlexNet256())
+    _feature_extractor = AlexNet256()
+    _model = models.NNModel(
+        _feature_extractor.feature_size,
+        state_dict_path="./models/basic_nn_2019-11-01_17:12:59.pth",
+        eval_mode=True,
+    )
+    create_submission(_model, CompetitionFeatureDataset(_feature_extractor))
