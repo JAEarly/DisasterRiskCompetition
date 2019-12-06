d9a24e4b70ea08e55f3edab5160de5931d58a146
Joseph Early <joseph.early@btinternet.com>
2019-12-06 22:57:01 +0000

    Implemented dataset augmentation based on rotations.

--- src/setup.py
+++ src/setup.py
@@ -4,8 +4,9 @@ from data.make_dataset import download_dataset, extract_dataset
 from data.image_extraction import extract_images
 from data.preprocess_images import preprocess_images_basic, validate_dataset
 from model_manager import ModelManager
+from data.augment_dataset import augment_dataset
 
-num_stages = 6
+num_stages = 7
 print("Starting setup")
 print("\n(1/" + str(num_stages) + " - Download Dataset)")
 download_dataset()
@@ -17,6 +18,8 @@ print("\n(4/" + str(num_stages) + " - Preprocess Images)")
 preprocess_images_basic()
 print("\n(5/" + str(num_stages) + " - Validate Dataset)")
 validate_dataset()
-print("\n(6/" + str(num_stages) + " - Downloading Models")
+print("\n(6/" + str(num_stages) + " - Augmenting Dataset)")
+augment_dataset()
+print("\n(7/" + str(num_stages) + " - Downloading Models")
 ModelManager().download_all()
 print("Done")
